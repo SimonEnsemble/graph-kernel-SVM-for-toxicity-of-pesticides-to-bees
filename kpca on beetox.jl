5854ce0b71caba6ebfb55fd4b75e98c5a5130c2d
Ping Yang <youngping16@gmail.com>
2022-01-18 09:29:54 -0800

    kpca on beetox (spend 4 mins)

--- kpca on beetox.jl
+++ kpca on beetox.jl
@@ -4,8 +4,31 @@ using RWK, LinearAlgebra, JLD2, ProgressMeter, UnicodePlots, MolecularGraph, CSV
 
 df_contact = CSV.read("new_smiles.csv", DataFrame)
 
-df_contact[:, "SMILES"]
+contact_tox_mol = [addhydrogens(smilestomol(smiles)) for smiles in df_contact[:, "SMILES"]]
 
-contact_tox_mol = [smilestomol(smiles) for smiles in df_contact[:, "SMILES"]]
+n_mol = length(contact_tox_mol)
 
-beetox_mol = atomsymbol.(contact_tox_mol)
+n_jobs = Int(n_mol*(n_mol-1)/2 + n_mol)
+
+pbar = Progress(n_jobs, 1)
+
+id_pair = 0
+K = zeros(n_mol, n_mol)
+for m = 1:n_mol
+	for n = m:n_mol
+		# compute grw_kernel between every two different cages
+		dpg = direct_product_graph(contact_tox_mol[m], contact_tox_mol[n], verbose = true)
+		K[m, n] = fixed_point_grw_kernel(dpg, 0.1, ϵ = 0.01)
+		K[n, m] = K[m, n]
+		jldsave("BeeToxK.jld2"; K)
+		next!(pbar)
+	end
+end
+
+all_ones = ones(size(K)) / size(K)[1]
+
+tildeK = K - all_ones * K - K * all_ones + all_ones * K * all_ones
+
+λₛ, Qₛ = eigen(tildeK)
+
+barplot(["λ$i" for i = 1:length(λₛ)], abs.(λₛ), title = "eigenvalues of ̃K")
