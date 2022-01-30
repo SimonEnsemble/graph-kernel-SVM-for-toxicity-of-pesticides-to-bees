035a097a4cb663af30f75166b08911974c5f6ef9
Cory Simon <cokes@Corys-MacBook-Pro.local>
2022-01-30 14:35:12 -0800

    minor comments on gram matrix building

--- Beetox Gram Matrix.jl
+++ Beetox Gram Matrix.jl
@@ -1,40 +1,38 @@
+push!(LOAD_PATH, joinpath(pwd(), "src"))
 using RWK, LinearAlgebra, JLD2, ProgressMeter, MolecularGraph, CSV, DataFrames
 
 # load BeeToxAI dataset
+data = CSV.read("new_smiles.csv", DataFrame)
 
-df_contact = CSV.read("new_smiles.csv", DataFrame)
+mols = [smilestomol(smiles) for smiles in data[:, "SMILES"]]
 
-contact_tox_mol = [smilestomol(smiles) for smiles in df_contact[:, "SMILES"]]
-
-addhydrogens_flag = true
-
+addhydrogens_flag = false
 if addhydrogens_flag
-	contact_tox_mol = addhydrogens.(contact_tox_mol)
+	mols = addhydrogens.(mols)
 end
 
-n_mol = length(contact_tox_mol)
+n_mol = length(mols)
 
-n_jobs = Int(n_mol*(n_mol-1)/2 + n_mol)
+n_jobs = Int(n_mol * (n_mol - 1) / 2 + n_mol)
 
 pbar = Progress(n_jobs, 1)
 
-K = zeros(n_mol, n_mol)
+K = zeros(n_mol, n_mol) # Gram matrix
 γ = 0.01
 for m = 1:n_mol
 	for n = m:n_mol
-		# compute grw_kernel between every two different cages
-		dpg = direct_product_graph(contact_tox_mol[m], contact_tox_mol[n], verbose = false)
+		dpg = direct_product_graph(mols[m], mols[n], verbose=false)
+
 		K[m, n] = grw_kernel(dpg, γ)
 		K[n, m] = K[m, n]
+
 		next!(pbar)
 	end
 end
 
-N = size(K)[1]
-
-C = I(N) - 1/N * ones(N, N)
-
-Kcenter = C*K*C
+# center the Gram matrix
+C = I(n_mol) - 1 / n_mol * ones(n_mol, n_mol)
+Kcenter = C * K * C
 
 if addhydrogens_flag
 	jldsave("BeeToxKwh.jld2"; Kcenter)
