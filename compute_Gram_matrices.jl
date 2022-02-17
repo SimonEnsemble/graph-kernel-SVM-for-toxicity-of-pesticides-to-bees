b2123d0d7a22c5e784923063f9b39cdae5c9acc5
Cory Simon <cokes@Corys-MacBook-Pro.local>
2022-02-16 17:48:57 -0800

    set up for single kernel

--- compute_Gram_matrices.jl
+++ compute_Gram_matrices.jl
@@ -6,13 +6,12 @@ using Distributed, SharedArrays, JLD2, MolecularGraph, CSV, DataFrames, Progress
 settings
 =#
 include_hydrogens = false
-kernels = [fixed_length_rw_kernel, grw_kernel]
-params = Dict(
-     # grw_kernel            => [0.05, 0.04, 0.03, 0.02, 0.01], # γ's
-     grw_kernel            => [],
-     # fixed_length_rw_kernel => [1, 2, 3, 4, 5] # l's
-     fixed_length_rw_kernel => [1] # l's
-)
+kernel = fixed_length_rw_kernel
+params = [1, 2] # l's
+# kernel = grw_kernel
+# params = [0.05, 0.04, 0.03, 0.02, 0.01], # γ's
+
+println("settings:\n\tkernel = ", kernel, "\n\tinclude_hydrogens = ", include_hydrogens)
 
 # set up directory for storing data
 savedir = "include_hydrogens_$include_hydrogens"
@@ -27,58 +26,35 @@ convert SMILES to graphs
 data = CSV.read("new_smiles.csv", DataFrame)
 toxicity = data[:, "Outcome"] # targets
 
-@everywhere mols = [smilestomol(smiles) for smiles in data[1:10, "SMILES"]]
+mols = [smilestomol(smiles) for smiles in data[1:10, "SMILES"]]
 if include_hydrogens
 	mols = addhydrogens.(mols)
 end
 
-# set up kernel
-@everywhere rwk = dpg -> fixed_length_rw_kernel(dpg, 2)
+n_mol = length(mols) # number of molecules
+println("\t# molecules = ", n_mol)
+for p in params
+    println("\t\tcomputing Gram matrix for kernel param = ", p)
 
-# compute gram matrix
-@everywhere n_mol = length(mols) # number of molecules
-pbar = Progress(n_mol, 1)
-K = SharedMatrix{Float64}(n_mol, n_mol) # Gram matrix
-@sync @distributed for m = 1:n_mol
-    for n = m:n_mol
-        dpg = direct_product_graph(mols[m], mols[n], verbose=false)
+    # set up kernel
+    rwk = dpg -> kernel(dpg, p)
 
-        K[m, n] = rwk(dpg)
-        K[n, m] = K[m, n]
+    # compute gram matrix
+    K = SharedMatrix{Float64}(n_mol, n_mol) # Gram matrix
+    @sync @distributed for m = 1:n_mol
+        for n = m:n_mol
+            dpg = direct_product_graph(mols[m], mols[n], verbose=false)
+
+            K[m, n] = rwk(dpg)
+            K[n, m] = K[m, n]
+        end
     end
+    K = Matrix(K)
+
+    # center the gram matrix
+    Kcentered = centered_Gram_matrix(K)
+    
+    # save to file
+    savename = "BeeTox_$(kernel)_$p.jld2"
+    jldsave(joinpath(savedir, savename); K, Kcentered, mols, toxicity)
 end
-println(Matrix(K))
-#=
-compute Gram matrix
-=#
-#for kernel in kernels
-#    println("kernel = ", kernel)
-#    for p in params[kernel]
-#        println("\tparam = ", p)
-#        # set up kernel
-#        rwk = dpg -> kernel(dpg, p)
-#        
-#        # compute gram matrix
-#        @everywhere n_mol = length(mols) # number of molecules
-#        pbar = Progress(n_mol, 1)
-#        K = SharedMatrix{Float64}(n_mol, n_mol) # Gram matrix
-#        @distributed for m = 1:n_mol
-#            #next!(pbar)
-#            for n = m:n_mol
-#                dpg = direct_product_graph(mols[m], mols[n], verbose=false)
-#
-#                K[m, n] = rwk(dpg)
-#                K[n, m] = K[m, n]
-#            end
-#        end
-#
-#        K = Matrix(K)
-#        
-#        # center the gram matrix
-#        Kcentered = centered_Gram_matrix(K)
-#        
-#        # save to file
-#        savename = "BeeTox_$(kernel)_$p.jld2"
-#        jldsave(joinpath(savedir, savename); K, Kcentered, mols, toxicity)
-#    end
-#end
