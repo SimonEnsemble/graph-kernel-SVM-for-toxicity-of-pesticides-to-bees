39739ace1d45612d74855d1a11d3a57967ca70a5
Ping Yang <youngping16@gmail.com>
2022-04-13 16:24:49 -0700

    add MACCSkey

--- fingerprint.py
+++ fingerprint.py
@@ -1,23 +1,32 @@
 from rdkit import Chem
 from rdkit import DataStructs
 from rdkit.Chem import AllChem
+from rdkit.Chem import MACCSkeys
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv("new_smiles.csv")
 
-fps = []
+fps_morgan = []
+fps_MACCS = []
 for smile in df["SMILES"]:
     m = Chem.MolFromSmiles(smile)
-    fp = AllChem.GetMorganFingerprint(m, 4)
-    fps.append(fp)
+    fp_morgan = AllChem.GetMorganFingerprint(m, 4)
+    fp_MACCS = MACCSkeys.GenMACCSKeys(m)
+    fps_morgan.append(fp_morgan)
+    fps_MACCS.append(fp_MACCS)
 
-n = len(fps)
-K = np.eye(n)
+n = len(fps_morgan)
+K_morgan = np.eye(n)
 for i in range(n):
     for j in range(n):
-        K[i, j] = DataStructs.DiceSimilarity(fps[i], fps[j])
-        K[j, i] = K[i, j]
+        K_morgan[i, j] = DataStructs.DiceSimilarity(fps_morgan[i], fps_morgan[j])
 
+np.save('MFDSK', K_morgan)
 
-np.save('MFDSK', K)
+K_maccs = np.eye(n)
+for i in range(n):
+    for j in range(n):
+        K_maccs[i, j] = DataStructs.FingerprintSimilarity(fps_MACCS[i],fps_MACCS[j])
+
+np.save('MACCSFSK', K_maccs)
