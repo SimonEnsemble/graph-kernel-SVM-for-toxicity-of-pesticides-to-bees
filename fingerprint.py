4513544ea0eed39c8afaab8577070e7dc4ca4b09
Cory Simon <cokes@10-249-160-254.wireless.oregonstate.edu>
2022-04-15 16:10:08 -0700

    consolidate for loop

--- fingerprint.py
+++ fingerprint.py
@@ -8,25 +8,21 @@ import numpy as np
 df = pd.read_csv("new_smiles.csv")
 
 fps_morgan = []
-fps_MACCS = []
+fps_MACCS  = []
 for smile in df["SMILES"]:
     m = Chem.MolFromSmiles(smile)
     fp_morgan = AllChem.GetMorganFingerprint(m, 4)
-    fp_MACCS = MACCSkeys.GenMACCSKeys(m)
+    fp_MACCS  = MACCSkeys.GenMACCSKeys(m)
     fps_morgan.append(fp_morgan)
     fps_MACCS.append(fp_MACCS)
 
 n = len(fps_morgan)
 K_morgan = np.eye(n)
-for i in range(n):
-    for j in range(n):
-        K_morgan[i, j] = DataStructs.TanimotoSimilarity(fps_morgan[i], fps_morgan[j])
-
-np.save('MFTSK', K_morgan)
-
 K_maccs = np.eye(n)
 for i in range(n):
     for j in range(n):
-        K_maccs[i, j] = DataStructs.TanimotoSimilarity(fps_MACCS[i],fps_MACCS[j])
+        K_morgan[i, j] = DataStructs.TanimotoSimilarity(fps_morgan[i], fps_morgan[j])
+        K_maccs[i, j]  = DataStructs.TanimotoSimilarity(fps_MACCS[i],fps_MACCS[j])
 
+np.save('MFTSK', K_morgan)
 np.save('MACCSTSK', K_maccs)
