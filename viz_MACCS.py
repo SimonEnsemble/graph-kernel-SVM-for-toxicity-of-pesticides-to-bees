f45e35361d7d51e840f106385cb11b44f1295c49
SimonEnsemble <cory.simon@oregonstate.edu>
2022-05-07 16:45:01 -0700

    simplify coloring

--- viz_MACCS.py
+++ viz_MACCS.py
@@ -13,20 +13,12 @@ smiles = [smile for smile in df["SMILES"]]
 hl_color = (0.4, 1.0, 1.0, 0.0) # first one is alpha
 grid_image_kwargs = {"molsPerRow": 4, "useSVG": True, "subImgSize": (250, 250)}
 
-def build_highlight_colors(mol, highlight, substructure):
-    hit_bonds = []
-    for bond in substructure.GetBonds():
-        aid1 = highlight[bond.GetBeginAtomIdx()]
-        aid2 = highlight[bond.GetEndAtomIdx()]
-        hit_bonds.append(mol.GetBondBetweenAtoms(aid1, aid2).GetIdx())
-
+def build_highlight_colors():
     atom_colors = {}
-    for i, at in enumerate(highlight):
-        atom_colors[at] = hl_color
-
     bond_colors = {}
-    for i, bd in enumerate(hit_bonds):
-        bond_colors[bd] = hl_color
+    for i in range(100):
+        atom_colors[i] = hl_color
+        bond_colors[i] = hl_color
     return atom_colors, bond_colors
 
 def get_mol(id_mol):
@@ -61,7 +53,7 @@ def viz_molecule(id_mol):
     #molsPerRow=4, subImgSize=(250, 250), useSVG=True)
     save_to_svg(svg_string, "mol_{}.svg".format(id_mol))
 
-def viz_maccs(id_mol, just_one=True):
+def viz_maccs(id_mol, just_one=False):
     mol = get_mol(id_mol)
 
     # compute maccs fingerprint
@@ -88,7 +80,7 @@ def viz_maccs(id_mol, just_one=True):
         # append these highlights to list of highlights for each molecule
         highlight_list.append(highlight)
     
-        atom_color, bond_color = build_highlight_colors(mol, highlight, substructure)
+        atom_color, bond_color = build_highlight_colors()
         atom_colors.append(atom_color)
         bond_colors.append(bond_color)
     svg_string = Draw.MolsToGridImage(mols, highlightAtomLists=highlight_list, **grid_image_kwargs, 
@@ -96,7 +88,7 @@ def viz_maccs(id_mol, just_one=True):
     save_to_svg(svg_string, "mol_w_highlights_{}.svg".format(id_mol))
     # save to SVG
 
-def viz_one_maccs(id_mol, onbit, just_one=True):
+def viz_one_maccs(id_mol, onbit, just_one=False):
     # get mol
     mol = get_mol(id_mol)
 
@@ -119,6 +111,6 @@ def viz_one_maccs(id_mol, onbit, just_one=True):
 
 
 id_mol = 347
-viz_maccs(id_mol, True)
+viz_maccs(id_mol, False)
 viz_molecule(id_mol)
 viz_one_maccs(10-1, 104)
