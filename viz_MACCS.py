2ef01cfa3d530c88bb07b9b91a04137b421f1df3
CorySimon <corysimon@berkeley.edu>
2022-05-09 15:48:45 -0700

    viz both

--- viz_MACCS.py
+++ viz_MACCS.py
@@ -11,7 +11,7 @@ df = pd.read_csv("new_smiles.csv")
 smiles = [smile for smile in df["SMILES"]]
 
 hl_color = (0.4, 1.0, 1.0, 0.0) # first one is alpha
-grid_image_kwargs = {"molsPerRow": 4, "useSVG": True, "subImgSize": (250, 250)}
+grid_image_kwargs = {"molsPerRow": 12, "useSVG": True, "subImgSize": (250, 250)}
 
 def build_highlight_colors():
     atom_colors = {}
@@ -85,7 +85,9 @@ def viz_maccs(id_mol, just_one=False):
         bond_colors.append(bond_color)
     svg_string = Draw.MolsToGridImage(mols, highlightAtomLists=highlight_list, **grid_image_kwargs, 
                                       highlightAtomColors=atom_colors, highlightBondColors=bond_colors)
-    save_to_svg(svg_string, "mol_w_highlights_{}.svg".format(id_mol))
+    svg_filename = "mol_w_highlights_{}_{}.svg".format(id_mol, just_one)
+    save_to_svg(svg_string, svg_filename)
+    print("see ", svg_filename)
     # save to SVG
 
 def viz_one_maccs(id_mol, onbit, just_one=False):
@@ -106,10 +108,13 @@ def viz_one_maccs(id_mol, onbit, just_one=False):
     highlight = get_highlight(mol, substructure, just_one)
     
     svg_string = Draw.MolsToGridImage([mol], highlightAtomLists=[highlight], **grid_image_kwargs)
-    save_to_svg(svg_string, "mol_w_highlights_{}_onbit_{}.svg".format(id_mol, onbit))
+    svg_filename = "mol_w_highlights_{}_onbit_{}_{}.svg".format(id_mol, onbit, just_one)
+    save_to_svg(svg_string, svg_filename)
+    print("see ", svg_filename)
 
 # rsvg-convert -f pdf -o mygraph.pdf mol_w_highlights_347.svg to convert svg to pdf
 
 id_mol = 347
 viz_maccs(id_mol, False)
+viz_maccs(id_mol, True)
 viz_molecule(id_mol)
